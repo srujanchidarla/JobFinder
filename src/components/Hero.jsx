0d092f851b00a4ba8ca0341fc25969824da84195
Srujan Chidarla <srujanvarmamng@gmail.com>
2024-10-20 17:25:27 -0400

    Added Routing for different pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
-const Hero = () => {
+const Hero = ({
+  title = "Become a React Dev",
+  subtitle = "Find the React job that fits your skill set",
+}) => {
   return (
-    <section className="bg-blue-50 py-4">
-      <div className="container mx-auto px-4">
-        <div className="relative">
-          <input
-            type="text"
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-            placeholder="Filter jobs..."
-          />
+    <section className="bg-indigo-700 py-20 mb-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+            {title}
+          </h1>
+          <p className="my-4 text-xl text-white">{subtitle}</p>
         </div>
       </div>
     </section>
   );
 };
-
 export default Hero;
